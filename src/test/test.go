18ecc72b001e7cdf1877983988910f6ede81b9aa
uestcwt2015 <uestcwutong@gmail.com>
2018-12-18 22:17:05 +0800

    exercise http

--- src/test/test.go
+++ src/test/test.go
@@ -1,40 +1,19 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "flag"
+    "strings"
+)
 
-func convert(s string, numRows int) string {
-	var ret = make([][]rune, 0)
-	var res = make([]rune, 0)
-	for i := 0; i < numRows; i++ {
-		child := make([]rune, len(s))
-		ret = append(ret, child)
-	}
-	x, y := 0, 0
-	for i, v := range s {
-		n := 2*numRows - 2
-		ret[y][x] = v
-		// fmt.Println(x, y, v, i%n < numRows-1)
-		if i%n < numRows-1 {
-			y++
-		} else {
-			x++
-			y--
-		}
-		// fmt.Println(x, y)
-	}
-	// fmt.Println(ret)
-	for i := 0; i < numRows; i++ {
-		length := len(ret[i])
-		for j := 0; j < length && len(res) < len(s); j++ {
-			if ret[i][j] != 0 {
-				res = append(res, ret[i][j])
-			}
-		}
-	}
-	fmt.Println(string(res))
-	return string(res)
-}
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
 
 func main() {
-	convert("PAYPALISHIRING", 3)
+    flag.Parse()
+    fmt.Print(strings.Join(flag.Args(), *sep))
+
+    if !*n {
+        fmt.Println()
+    }
 }
